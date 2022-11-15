24cad2fd8d9ee525afbb13d380cf7bb0f61e044e
Gaurav Sutar <83762168+gauravstr07@users.noreply.github.com>
2022-11-16 00:44:48 +0530

    NodeJs MongoDb Authentication with CryptoJs

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
 const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
 
@@ -21,6 +22,7 @@ mongoose
 app.use(express.json());
 
 app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
 
 app.listen(port, () => {
   console.log(`server running on port: ${port}📡`);
